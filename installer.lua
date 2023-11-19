aca238ddb600a7e7331700f8a40f95f6acd81130
ss123she <130488098+ss123she@users.noreply.github.com>
2023-11-19 21:38:23 +0300

    Create installer.lua

---
+++ installer.lua
@@ -0,0 +1,26 @@
+if fs.exists("/ar-base.lua") then
+    term.write("\nApplying an update...")
+    os.sleep(1)
+    term.write("\nDeleted old ar-base.lua...")
+    fs.delete("/ar-base.lua")
+    os.sleep(1)
+end
+if fs.exists("/startup.lua") then
+    term.write("\nDeleted old startup.lua...")
+    fs.delete("/startup.lua")
+    os.sleep(1)
+end
+term.write("\nInstalling ar-base.lua...")
+os.sleep(1)
+shell.run("wget https://raw.githubusercontent.com/ss123she/CC_AR/main/ar-base.lua")
+os.sleep(1)
+shell.run("wget https://raw.githubusercontent.com/ss123she/CC_AR/main/startup.lua")
+os.sleep(1)
+term.write("\nRebooting...")
+os.sleep(1)
+term.write("progress: [███████----------]")
+os.sleep(1)
+term.write("progress: [█████████████----]")
+os.sleep(1)
+term.write("progress: [█████████████████]")
+os.reboot()
