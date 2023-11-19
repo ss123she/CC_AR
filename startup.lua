c51ff047e83900d64bcfa1c3aac5ebff7e607018
ss123she <gamegames8293@gmail.com>
2023-11-19 22:55:39 +0300

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,64 +1,29 @@
-local internet = require("internet")
-local ltn12 = require("ltn12")
-local fs = require("filesystem")
-local shell = require("shell")
-
--- Замените эти значения на свои данные
-local githubUsername = "ss123she"
-local githubRepository = "CC_AR"
-local fileName = "base.lua"
-local localFilePath = fileName
-
--- Функция для загрузки содержимого файла с GitHub
-function downloadFromGitHub(username, repository, file, outputFilePath)
-    local url = string.format("https://raw.githubusercontent.com/%s/%s/main/%s", username, repository, file)
-    local response = {}
-
-    local success, status = pcall(function()
-        for chunk in internet.request(url) do
-            table.insert(response, chunk)
-        end
-    end)
-
-    if success and status == 200 then
-        local fileContent = table.concat(response)
-        local file = io.open(outputFilePath, "w")
-        file:write(fileContent)
-        file:close()
-        print("File downloaded successfully.")
-    else
-        print("Failed to download file.")
-    end
+function wget(link)
+    shell.run("wget " + link)
 end
 
--- Функция для сравнения двух файлов
-function compareFiles(localFile, githubFile)
-    local localContent = assert(io.open(localFile)):read("*all")
-    local githubContent = githubFile
+function compareFiles(file1, file2)
+    local content1 = assert(io.open(file1, "rb")):read("*all")
+    local content2 = assert(io.open(file2, "rb")):read("*all")
 
-    return localContent == githubContent
+    return content1 == content2
 end
-
--- Проверяем, существует ли локальный файл
-if fs.exists(localFilePath) then
-    -- Загружаем содержимое файла с GitHub
-    local githubContent = downloadFromGitHub(githubUsername, githubRepository, fileName, localFilePath)
-
-    -- Сравниваем локальный и удаленный файлы
-    if not compareFiles(localFilePath, githubContent) then
-        print("Local and remote files are different. Updating...")
-        downloadFromGitHub(githubUsername, githubRepository, fileName, localFilePath)
-    else
-        print("Local and remote files are the same. No update needed.")
+if fs.exists("/bin") then
+    shell.run("cd /bin")
+    if fs.exists("/bin/base.lua") then
+        fs.remove("/bin/base.lua")
     end
+    wget("https://raw.githubusercontent.com/ss123she/CC_AR/main/base.lua")
+    shell.write("success")
+    shell.run("cd /")
 else
-    print("Local file does not exist. Downloading from GitHub...")
-    downloadFromGitHub(githubUsername, githubRepository, fileName, localFilePath)
+    shell.run("mkdir /bin")
+    shell.write("folder bin created")
+    shell.run("cd /bin")
+    wget("https://raw.githubusercontent.com/ss123she/CC_AR/main/base.lua")
+    shell.write("success")
+    shell.run("cd /")
 end
+if fs.exists("/installer.lua") then fs.remove("/installer.lua") end
 
--- Удаляем старый установочный файл, если существует
-if fs.exists("/installer.lua") then
-    fs.remove("/installer.lua")
-end
--- Запускаем основной файл
 shell.execute("/base.lua")
